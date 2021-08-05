0bfa07238aaa00d4ab7eea4a3748603f310a2656
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-05 23:56:06 +0600

    initialized with math file

---
+++ math.js
@@ -0,0 +1,17 @@
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+
+const myNumber = 2.6398;
+// const output = Math.ceil(myNumber);
+// const output = Math.floor(myNumber);
+// const output = Math.round(myNumber);
+
+// const output = Math.random()*6 ;
+// const rounded = Math.round(output);
+// console.log(rounded);
+
+for(let i = 0; i < 20; i++){
+    const output = Math.random() * 6;
+    const rounded = Math.round(output);
+    console.log(rounded);
+}
